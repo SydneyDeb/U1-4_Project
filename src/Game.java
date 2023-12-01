9f9aa4d9991c6504aa716ba34861a0aeb5026d99
Sydney Deb <sydneyd30@nycstudents.net>
2023-12-01 00:42:32 -0500

    added a method

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,85 @@
-package PACKAGE_NAME;public class Game {
+public class Game {
+    private int lives;
+    private String word;
+    private String difficulty;
+
+
+    public Game(int lives, String difficulty) {
+        this.lives = lives;
+        this.difficulty = difficulty;
+    }
+
+    public Game() {
+        lives = 6;
+        difficulty = "easy";
+    }
+
+    public String wordGen() {
+        int num = (int) (Math.random() * 5) + 1;
+        if (difficulty.equals("easy")) {
+            if (num == 1) {
+                word = "bean";
+            }
+            else if (num == 2) {
+                word = "dark";
+            }
+            else if (num == 3) {
+                word = "vain";
+            }
+            else if (num == 4) {
+                word = "idol";
+            }
+            else {
+                word = "acai";
+            }
+        }
+        else {
+            if (num == 1) {
+                word = "kitchen";
+            }
+            else if (num == 2) {
+                word = "edition";
+            }
+            else if (num == 3) {
+                word = "country";
+            }
+            else if (num == 4) {
+                word = "genetic";
+            }
+            else {
+                word = "stomach";
+            }
+        }
+        return word;
+    }
+
+    public int getLives () {
+        return lives;
+    }
+
+    public String toString() {
+        return "You have " + lives + " lives and you are on " + difficulty + " mode.";
+    }
+
+
+    public String revealGuess(String blank, String guess) {
+        String str = word;
+        int index = str.indexOf(guess);
+        while (index != -1) {
+            blank = blank.substring(0, index) + guess + blank.substring(index + 1);
+            str = str.substring(0, index) + "*" + str.substring(index + 1);
+            index = str.indexOf(guess);
+        }
+        return blank;
+    }
+
+    public void blankChecker(String blank, String word) {
+        if (blank.contains("_")) {
+            System.out.println("You lost! Better luck next time.");
+            System.out.println("The mystery word was: " + word);
+        }
+        else {
+            System.out.println("You Win! Great work.");
+        }
+    }
 }
--- src/GameRunner.java
+++ src/GameRunner.java
@@ -1,2 +1,64 @@
-package PACKAGE_NAME;public class GameRunner {
+import java.util.Scanner;
+public class GameRunner {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome to Word Guesser! Enter letters to make up the mystery word before you loose all your lives!");
+        System.out.println("Would difficulty would you like to play, please enter \"basic\" or \"hard\": ");
+        String mode = scan.nextLine();
+        boolean basicMode = mode.equals("basic");
+
+
+        if (basicMode) {
+            Game g = new Game();
+            String word = g.wordGen();
+            int lives = g.getLives();
+            System.out.println("Game loading...");
+            System.out.println(g);
+            String blank = "____";
+            System.out.println(blank);
+
+
+            while (lives > 0 && blank.contains("_")) {
+                System.out.println("Please enter a letter in lowercase");
+                String guess = scan.nextLine();
+                if (word.contains(guess)) {
+                    System.out.println("This letter is in the mystery word!");
+                    blank = g.revealGuess(blank, guess);
+                    System.out.println(blank);
+                } else {
+                    lives--;
+                    System.out.println("This letter is not in the mystery word. -1 life");
+                    System.out.println("You have " + lives + " lives left.");
+                    System.out.println(blank);
+                }
+            }
+            g.blankChecker(blank, word);
+
+        } else {
+            Game g2 = new Game(5, "hard");
+            String word = g2.wordGen();
+            int lives = g2.getLives();
+            System.out.println("Game loading...");
+            System.out.println(g2);
+            String blank = "_______";
+            System.out.println(blank);
+
+
+            while (lives > 0 && blank.contains("_")) {
+                System.out.println("Please enter a letter in lowercase");
+                String guess = scan.nextLine();
+                if (word.contains(guess)) {
+                    System.out.println("This letter is in the mystery word!");
+                    blank = g2.revealGuess(blank, guess);
+                    System.out.println(blank);
+                } else {
+                    lives--;
+                    System.out.println("This letter is not in the mystery word. -1 life");
+                    System.out.println("You have " + lives + " lives left.");
+                    System.out.println(blank);
+                }
+            }
+            g2.blankChecker(blank, word);
+        }
+    }
 }
